6a8738de55a1f6e4986e4113dd82e6d651fbe75b
Miguel Ferreira <mferreira@shubergphilis.com>
2014-03-24 18:04:49 +0100

    Set mac keyboard variant

--- ubuntu/run-ff-xephyr.sh
+++ ubuntu/run-ff-xephyr.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-Xephyr -keybd ephyr,,,xkbmodel=pc105,xkblayout=pt,xkbrules=evdev -screen 1680x1020 :1 & 
+Xephyr -keybd ephyr,,,xkbmodel=pc105,xkblayout=pt,xkbvariant=mac,xkbrules=evdev -screen 1680x1020 :1 & 
 sleep 2
 DISPLAY=:1
 
